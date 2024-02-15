bd6524f4f0f51b23f16a24674fa298c433a489f3
Vinicius wessner <67404041+ViniciusWessner@users.noreply.github.com>
2024-02-15 20:19:56 -0300

    Autenticacao do usuario

---
+++ Vollmed/Services/UserDefaultsHelper.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultsHelper.swift
+//  Vollmed
+//
+//  Created by Vinicius Wessner on 14/02/24.
+//
+
+import Foundation
---
+++ Vollmed/Views/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  Vollmed
+//
+//  Created by Vinicius Wessner on 14/02/24.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
